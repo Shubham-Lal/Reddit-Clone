e038cc5b37ac219cdbaccd91979f1788c90162d3
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-02-16 23:59:24 +0530

    Initial Commit

---
+++ src/chakra/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme } from "@chakra-ui/react"
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF3C00"
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+        body: {
+            bg: "gray.200",
+        },
+    }),
+  },
+  components: {
+    // Button
+  }
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import SEO from './seo';
+
+export default function Home() {
+  return (
+    <>
+      <SEO />
+      <div>
+        Hello
+      </div>
+    </>
+  )
+}
