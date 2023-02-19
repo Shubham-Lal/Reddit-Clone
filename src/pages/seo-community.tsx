adc9d7d83adc0c1e8fbc3fa9ffc671b3e54de8ca
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-02-19 15:07:04 +0530

    Working on Compile errors

--- src/pages/seo-community.tsx
+++ src/pages/seo-community.tsx
@@ -10,8 +10,8 @@ const CommunitySEO:React.FC<CommunitySEOProps> = ({ CommunityData }) => {
     
     return (
         <Head>
-            <title>{CommunityData.id}</title>
-            <meta name="description" content={`${CommunityData.id}'s Community Page | Reddit Clone`} />
+            <title>{CommunityData?.id}</title>
+            <meta name="description" content={`${CommunityData?.id}'s Community Page | Reddit Clone`} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href={seoImg} />
         </Head>
