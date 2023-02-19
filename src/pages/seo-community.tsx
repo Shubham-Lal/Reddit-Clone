600218d252bb24720a2ad2bf775f5655b5d57c37
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-02-19 15:42:15 +0530

    Working on Website Image

--- src/pages/seo-community.tsx
+++ src/pages/seo-community.tsx
@@ -6,14 +6,13 @@ type CommunitySEOProps = {
 };
 
 const CommunitySEO:React.FC<CommunitySEOProps> = ({ CommunityData }) => {
-    const seoImg = CommunityData?.imageURL ? CommunityData?.imageURL : "/favicon.ico";
     
     return (
         <Head>
             <title>{CommunityData?.id}</title>
             <meta name="description" content={`${CommunityData?.id}'s Community Page | Reddit Clone`} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href={seoImg} />
+            <link rel="icon" href={"/favicon.ico" || CommunityData?.imageURL} />
         </Head>
     )
 }
