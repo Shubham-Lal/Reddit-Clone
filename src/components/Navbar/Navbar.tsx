76a1bedd294340d2c819a088c1c5e56a482fd3de
Shubham Lal <lal.shubham027@gmail.com>
2023-05-24 12:01:39 +0530

    Removed Search element

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar: React.FC = () => {
                     }} />
                 </Flex>
                 {user && <Directory />}
-                <SearchInput user={user} />
+                {/* <SearchInput user={user} /> */}
                 <RightContent user={user} />
             </Flex>
         </>
